463a1602cad793a2c8165d95efd0eb622d54007f
sichoi42 <42.4.sichoi@gmail.com>
2022-11-13 17:20:59 +0900

    :sparkles: add JwtModule #2
    add JwtModule #2

---
+++ backend/src/auth/auth.controller.ts
@@ -0,0 +1,8 @@
+import { Controller, Get } from '@nestjs/common';
+
+@Controller('')
+export class AuthController {
+  constructor(){}
+
+
+}
---
+++ backend/src/auth/auth.module.ts
@@ -0,0 +1,22 @@
+import { Module } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { JwtModule } from '@nestjs/jwt';
+import { AuthController } from './auth.controller';
+
+@Module({
+  imports: [
+    JwtModule.registerAsync({
+      useFactory: async (configService: ConfigService) => ({
+        secret: configService.get<string>('jwt.secret'),
+        signOptions: {
+          expiresIn: configService.get<string>('jwt.expiresIn'),
+        },
+      }),
+      inject: [ConfigService],
+    }),
+  ],
+  providers: [],
+  controllers: [AuthController],
+  exports: [],
+})
+export class AuthModule {}

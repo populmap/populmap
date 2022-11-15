55a7a3f9897f1d463d94c56dd7a040d9ad5af397
aseungbo <seungbo1112@gmail.com>
2022-11-15 17:26:00 +0900

    :sparkles: feat: Apply Axios

---
+++ frontend/src/network/axios/axios.custom.ts
@@ -0,0 +1,24 @@
+import instance from "./axios.instance";
+
+// const axiosGetExampleUrl = "/api/get";
+
+// export const axiosGetExample = async (): Promise<any> => {
+//   try {
+//     const response = await instance.get(axiosGetExampleUrl);
+//     return response;
+//   } catch (error) {
+//     throw error;
+//   }
+// };
+
+// const axiosPostExampleUrl = "/api/post";
+// export const axiosPostExample = async (): Promise<any> => {
+//   try {
+//     const response = await instance.post(axiosPostExampleUrl, {
+//       data,
+//     });
+//     return response;
+//   } catch (error) {
+//     throw error;
+//   }
+// };

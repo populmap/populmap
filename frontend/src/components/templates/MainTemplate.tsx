d2710d1517860d0c6e5acc05cb3157be891dc1e2
aseungbo <seungbo1112@gmail.com>
2022-11-15 20:31:54 +0900

    :sparkles: feat: Add NavTemplate

--- frontend/src/components/templates/MainTemplate.tsx
+++ frontend/src/components/templates/MainTemplate.tsx
@@ -1,11 +1,13 @@
 import styled from "@emotion/styled";
 import InitMap from "../map/InitMap";
 import ContentTemplate from "./ContentTemplate";
+import NavTemplate from "./NavTemplate";
 
 const MainTemplate = () => {
   return (
     <ContentTemplate>
-      <InitMap />
+      {/* <InitMap /> */}
+      <NavTemplate />
     </ContentTemplate>
   );
 };

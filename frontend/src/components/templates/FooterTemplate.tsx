1b73cb840cf2e09e9db3d1ab84c88993d5234369
aseungbo <seungbo1112@gmail.com>
2022-11-15 21:32:56 +0900

    :sparkles: feat: Add FooterTemplate

---
+++ frontend/src/components/templates/FooterTemplate.tsx
@@ -0,0 +1,26 @@
+import style from "@emotion/styled";
+
+const Footer = style.footer`
+  font-weight: 400;
+  font-size: 0.5rem;
+  text-align: center;
+`;
+
+const FooterTemplate = (): JSX.Element => {
+  return (
+    <Footer>
+      <section>
+        <a
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://github.com/populmap/populmap"
+        >
+          &nbsp;Github&nbsp;
+        </a>
+      </section>
+      <section>© 2022 populmap </section>
+    </Footer>
+  );
+};
+
+export default FooterTemplate;

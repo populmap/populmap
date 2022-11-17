9fb515b84b20238a86ff454d9b04d38b3fe6dba7
aseungbo <seungbo1112@gmail.com>
2022-11-17 20:44:26 +0900

    :sparkles: feat: Add MarkedMap organism component && Remove example maps

--- frontend/src/components/organisms/MarkedMap.tsx
+++ frontend/src/components/organisms/MarkedMap.tsx
@@ -1,7 +1,7 @@
 import { Map, MapMarker } from "react-kakao-maps-sdk";
 import { useState, useEffect } from "react";
 
-const GeolocationMap = (): JSX.Element => {
+const MarkedMap = (): JSX.Element => {
   const [state, setState] = useState({
     center: {
       lat: 37.4882,
@@ -13,7 +13,6 @@ const GeolocationMap = (): JSX.Element => {
 
   useEffect(() => {
     if (navigator.geolocation) {
-      // GeoLocation을 이용해서 접속 위치를 얻어옵니다
       navigator.geolocation.getCurrentPosition(
         (position) => {
           setState((prev) => ({
@@ -34,7 +33,6 @@ const GeolocationMap = (): JSX.Element => {
         }
       );
     } else {
-      // HTML5의 GeoLocation을 사용할 수 없을때 마커 표시 위치와 인포윈도우 내용을 설정합니다
       //   setState((prev) => ({
       //     ...prev,
       //     errMsg: "geolocation을 사용할수 없어요..",
@@ -44,18 +42,19 @@ const GeolocationMap = (): JSX.Element => {
   }, []);
 
   return (
-    <Map // 지도를 표시할 Container
+    <Map
       center={state.center}
       style={{
-        // 지도의 크기
         width: "100%",
-        height: "450px",
+        height: "95vh",
+        position: "absolute",
+        zIndex: "0",
       }}
-      level={3} // 지도의 확대 레벨
+      level={3}
     >
       {!state.isLoading && (
         <MapMarker position={state.center}>
-          <div style={{ padding: "5px", color: "#000" }}>
+          <div style={{ padding: "0.5rem", color: "#000" }}>
             {state.errMsg ? state.errMsg : "여기에 계신가요?!"}
           </div>
         </MapMarker>
@@ -64,4 +63,4 @@ const GeolocationMap = (): JSX.Element => {
   );
 };
 
-export default GeolocationMap;
+export default MarkedMap;

cee916a034078457ea99d97554586eb6100c915c
aseungbo <seungbo1112@gmail.com>
2022-11-18 23:35:12 +0900

    :sparkles: feat: Modified kakao API call by useInjectKakaoMapApi

--- frontend/src/components/templates/MainTemplate.tsx
+++ frontend/src/components/templates/MainTemplate.tsx
@@ -1,17 +1,25 @@
 import styled from "@emotion/styled";
-import MarkedMap from "../organisms/MarkedMap";
+import { useInjectKakaoMapApi } from "react-kakao-maps-sdk";
+import LoadMap from "../atoms/map/LoadMap";
+import MapNav from "../organisms/MapNav";
 
 const MainSection = styled.section`
   position: relative;
-  height: 95vh;
-  width: 100vw;
+  height: 100%;
+  width: 100%;
 `;
 
 const MainTemplate = (): JSX.Element => {
+  // KAKAO MAP API 호출
+  const { loading, error } = useInjectKakaoMapApi({
+    appkey: `${import.meta.env.VITE_KAKAO_MAP_KEY}`,
+  });
+
   return (
-    // <MainSection>
-    <MarkedMap />
-    // </MainSection>
+    <MainSection>
+      {loading ? null : <LoadMap />}
+      <MapNav />
+    </MainSection>
   );
 };
 

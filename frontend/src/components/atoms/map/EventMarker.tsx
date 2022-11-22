f4e82758024129d1d79fe59d07ffbe60891c9329
aseungbo <seungbo1112@gmail.com>
2022-11-22 20:02:50 +0900

    :sparkles: feat: Modified EventMarker logic

--- frontend/src/components/atoms/map/EventMarker.tsx
+++ frontend/src/components/atoms/map/EventMarker.tsx
@@ -1,31 +1,37 @@
-import { MapMarker, useMap } from "react-kakao-maps-sdk";
-import { useState } from "react";
+import { MapMarker } from "react-kakao-maps-sdk";
+import { SetStateAction, Dispatch } from "react";
+import EventSummaryOverlay from "./EventSummaryOverlay";
 import { EventBasicInfoResponseDto } from "../../../types/dto/EventBasicInfoResponse.dto";
 
 interface EventMarkerProps {
   eventInfo: EventBasicInfoResponseDto;
+  currentMarker: number;
+  setCurrentMarker: Dispatch<SetStateAction<number>>;
 }
 
 const EventMarker = (props: EventMarkerProps): JSX.Element => {
-  const { eventInfo } = props;
-  const map = useMap();
-  const [isVisible, setIsVisible] = useState(false);
+  const { eventInfo, currentMarker, setCurrentMarker } = props;
+
   return (
-    <MapMarker
-      position={{
-        lat: eventInfo.lat,
-        lng: eventInfo.lng,
-      }}
-      onClick={(marker) => map.panTo(marker.getPosition())}
-      onMouseOver={() => setIsVisible(true)}
-      onMouseOut={() => setIsVisible(false)}
-    >
-      {isVisible && (
-        <div>
-          <span>{eventInfo.title}</span>
-        </div>
+    <>
+      <MapMarker
+        position={{
+          lat: eventInfo.lat,
+          lng: eventInfo.lng,
+        }}
+        onClick={(): void => {
+          setCurrentMarker(eventInfo.eventId);
+        }}
+      />
+      {currentMarker === eventInfo.eventId && (
+        <EventSummaryOverlay
+          position={{
+            lat: eventInfo.lat,
+            lng: eventInfo.lng,
+          }}
+        />
       )}
-    </MapMarker>
+    </>
   );
 };
 
--- frontend/src/components/organisms/LoadMap.tsx
+++ frontend/src/components/organisms/LoadMap.tsx
@@ -1,4 +1,5 @@
 import { Map } from "react-kakao-maps-sdk";
+import { useState } from "react";
 import { useAppSelector } from "../../redux/hook";
 import { EventBasicInfoResponseDto } from "../../types/dto/EventBasicInfoResponse.dto";
 import EventMarker from "../atoms/map/EventMarker";
@@ -10,6 +11,8 @@ interface LoadMapProps {
 const LoadMap = (props: LoadMapProps): JSX.Element => {
   const { eventMarkers } = props;
   const mapState = useAppSelector((state) => state.map);
+  const [currentMarker, setCurrentMarker] = useState<number>(-1);
+
   return (
     <Map
       center={{
@@ -23,9 +26,19 @@ const LoadMap = (props: LoadMapProps): JSX.Element => {
         zIndex: "0",
       }}
       level={mapState.level}
+      onClick={(): void => {
+        setCurrentMarker(-1);
+      }}
     >
       {eventMarkers.map((eventInfo) => {
-        return <EventMarker key={eventInfo.eventId} eventInfo={eventInfo} />;
+        return (
+          <EventMarker
+            key={eventInfo.eventId}
+            eventInfo={eventInfo}
+            currentMarker={currentMarker}
+            setCurrentMarker={setCurrentMarker}
+          />
+        );
       })}
     </Map>
   );

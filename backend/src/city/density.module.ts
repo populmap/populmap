d1d3efa9330b58826c0a599c1ab20d5cba1c2f42
sichoi42 <42.4.sichoi@gmail.com>
2022-12-01 16:14:39 +0900

    :construction: ongoing setting city module
    ongoing setting city module

---
+++ backend/src/city/density.module.ts
@@ -0,0 +1,18 @@
+// import { Module } from "@nestjs/common";
+// import { TypeOrmModule } from "@nestjs/typeorm";
+
+
+// const repo = {
+//   provide: 'ICityRepository',
+//   useClass: CityRepository,
+// };
+
+// @Module({
+//   imports: [
+//     TypeOrmModule.forFeature([City, CityPeople, CityRoad, CityAccident]),
+//   ],
+//   providers: [DensityService, repo],
+//   controllers: [DensityController],
+//   exports: [DensityService],
+// })
+// export class AuthModule {}

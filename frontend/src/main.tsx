9ddb3c2fa00386a54e8ca40b3dd841c5e78f39fb
aseungbo <seungbo1112@gmail.com>
2022-12-07 18:39:35 +0900

    :sparkles: feat: Apply redux persist

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,17 +3,23 @@ import ReactDOM from "react-dom/client";
 import { ThemeProvider } from "@mui/material/styles";
 import { Provider } from "react-redux";
 import { CookiesProvider } from "react-cookie";
+import { persistStore } from "redux-persist";
+import { PersistGate } from "redux-persist/integration/react";
 import muiCustomTheme from "./themes/muiCustomTheme";
 import store from "./redux/store";
 import App from "./App";
+
+const persistor = persistStore(store);
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <CookiesProvider>
       <Provider store={store}>
-        <ThemeProvider theme={muiCustomTheme}>
-          <App />
-        </ThemeProvider>
+        <PersistGate loading={null} persistor={persistor}>
+          <ThemeProvider theme={muiCustomTheme}>
+            <App />
+          </ThemeProvider>
+        </PersistGate>
       </Provider>
     </CookiesProvider>
   </React.StrictMode>

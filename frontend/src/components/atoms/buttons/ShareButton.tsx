a7f40fed84873a0f5737de1b7931d9f0cc33fb32
seungb5 <seungbo1112@gmail.com>
2022-12-26 14:57:43 +0900

    Fix: Apply Prettier

--- frontend/src/components/atoms/buttons/ShareButton.tsx
+++ frontend/src/components/atoms/buttons/ShareButton.tsx
@@ -1,26 +1,31 @@
 import IconButton from "@mui/material/IconButton";
-import IosShareIcon from '@mui/icons-material/IosShare';
+import IosShareIcon from "@mui/icons-material/IosShare";
 
 const ShareButton = (): JSX.Element => {
-    const handleClick = async () => {
-        try {
-            await navigator.clipboard.writeText(window.location.href);
-            alert("주소가 복사되었습니다.");
-        } catch (e) {
-            alert("복사에 실패했습니다.");
-        }
+  const handleClick = async () => {
+    try {
+      await navigator.clipboard.writeText(window.location.href);
+      alert("주소가 복사되었습니다.");
+    } catch (e) {
+      alert("복사에 실패했습니다.");
     }
-    
-    return (
-        <IconButton style={{flexDirection: "column",
+  };
+
+  return (
+    <IconButton
+      style={{
+        flexDirection: "column",
         fontSize: "0.5rem",
         height: "1.5rem",
         width: "3rem",
-        color: "black",}} onClick={handleClick}>
-          <IosShareIcon />
-          공유
-        </IconButton>
-      );
-}
+        color: "black",
+      }}
+      onClick={handleClick}
+    >
+      <IosShareIcon />
+      공유
+    </IconButton>
+  );
+};
 
 export default ShareButton;

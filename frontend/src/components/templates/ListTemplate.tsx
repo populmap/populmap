5f7aa148ba316f028cd8aea49e4fceee5f2f6599
seungb5 <seungbo1112@gmail.com>
2022-12-26 14:39:55 +0900

    Fix: Modified Build error

--- frontend/src/components/templates/ListTemplate.tsx
+++
@@ -1,9 +0,0 @@
-// const ListTemplate = (): JSX.Element => {
-//     return (
-//         // Select organism
-//         // List organism
-//         // Pagination organism
-//     )
-// }
-
-// export default ListTemplate;
